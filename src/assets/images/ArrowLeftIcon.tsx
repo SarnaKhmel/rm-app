9ae126f892666b3e35cc8ada32d1941a4b8fecea
sarnakhmel <OlSa@olsa.local>
2023-11-08 18:25:12 +0200

    add assets, add structure

---
+++ src/assets/images/ArrowLeftIcon.tsx
@@ -0,0 +1,14 @@
+export const ArrowLefticon = () => (
+    <svg
+        width="9"
+        height="13"
+        viewBox="0 0 9 13"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M8.22192 2.20624L3.62817 6.79999L8.22192 11.3937L6.81567 12.8L0.815674 6.79999L6.81567 0.799988L8.22192 2.20624Z"
+            fill="#272B33"
+            fill-opacity="0.6"
+        />
+    </svg>
+);
