bcc9576f3fa439bf7ac5da3b3f2f17a2ef0f1c20
sarnakhmel <OlSa@olsa.local>
2023-11-09 17:22:17 +0200

    fix component SelectBlock

--- src/components/FilterBlock/SelectBlock/SelectBlock.tsx
+++ src/components/FilterBlock/SelectBlock/SelectBlock.tsx
@@ -15,23 +15,85 @@ const SelectInput: React.FC = () => {
         dispatch(fetchLocation() as any);
     }, [dispatch]);
 
+    const getUniqueValues = (key: string, data: any[]) => {
+        const uniqueValues = new Set(data.map((item: any) => item[key]));
+        return Array.from(uniqueValues);
+    };
+
+    const optionsWithLabels = [
+        {
+            label: "Location",
+            value: "location",
+            children: [
+                {
+                    label: "Add Name",
+                    value: "Name",
+                    children: getUniqueValues("name", locations).map((locationName) => ({
+                        label: locationName,
+                        value: locationName,
+                        children: [],
+                    })),
+                },
+                {
+                    label: "Add Type",
+                    value: "type",
+                    children: getUniqueValues("type", locations).map((type) => ({
+                        label: type,
+                        value: type,
+                        children: [],
+                    })),
+                },
+                {
+                    label: "Add Dimension",
+                    value: "dimension",
+                    children: getUniqueValues("dimension", locations).map((dimension) => ({
+                        label: dimension,
+                        value: dimension,
+                        children: [],
+                    })),
+                },
+            ],
+        },
+        {
+            label: "Episode",
+            value: "episode",
+            children: [
+                {
+                    label: "Add Name",
+                    value: "episodeName",
+                    children: episodes.map((childEpisodesItem: any) => ({
+                        label: childEpisodesItem.episodeName,
+                        value: childEpisodesItem.episodeName,
+                        children: [],
+                    })),
+                },
+                {
+                    label: "Add Episode Number",
+                    value: "EpisodeNumber",
+                    children: episodes.map((childEpisodesItem: any) => ({
+                        label: childEpisodesItem.id,
+                        value: childEpisodesItem.id,
+                        children: [],
+                    })),
+                },
+            ],
+        },
+        {
+            label: "Character",
+            value: "character",
+            children: [],
+        },
+    ];
+
 
     return (
         <div>
-
-            {episodes.map(episode => (
-                <li key={episode.id}>
-                    {episode.episodeName}
-                </li>
-            ))}
-
-
-            --------------------------------
-            {locations.map(location => (
-                <li key={location.id}>
-                    {location.locationName}
-                </li>
-            ))}
+            <Cascader
+                style={{ width: "100%" }}
+                options={optionsWithLabels}
+                multiple
+                maxTagCount="responsive"
+            />
         </div>
     );
 };
